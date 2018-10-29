2130ad31accf7b00f1976d7c99d54197a6f0c7e1
Viacheslav Tradunskyi <v.tradunskih@gmail.com>
2018-10-29 23:16:18 +0200

    Simplify kotlin example. Remove parent, properties

--- kotlin/pom.kts
+++ kotlin/pom.kts
@@ -1,29 +1,25 @@
 project {
     name = "Polyglot :: Kotlin"
-    parent = "io.takari.polyglot:polyglot:0.3.3-SNAPSHOT"
+    groupId = "io.takari.polyglot"
     artifactId = "regular-project"
+    version = "0.3.3-SNAPSHOT"
     packaging = jar
 
     val junitVersion = 4.12
-
-    properties {
-        "project.build.sourceEncoding" assign "UTF-8"
-        "junit.version" += 4.12
-        "kotlin.version" assign "1.1.61"
-    }
+    val kotlinVersion = "1.1.61"
 
     dependencies {
-        compile("org.jetbrains.kotlin:kotlin-stdlib:" + it["kotlin.version"])
+        compile("org.jetbrains.kotlin:kotlin-stdlib:$kotlinVersion")
         compile(it.groupId + ":polyglot-common:" + it.version)
                 .exclusions("org.slf4j:jul-logger")
 
         test(
                 "junit:junit:$junitVersion" exclusions "org.hamcrest:hamcrest-core",
-                "org.jetbrains.kotlin:kotlin-test-junit:${it["kotlin.version"]}"
+                "org.jetbrains.kotlin:kotlin-test-junit:$kotlinVersion"
         )
         provided(
                 "io.takari.polyglot:polyglot-kotlin:0.3.3-SNAPSHOT",
-                "org.apache.maven.plugin-tools:maven-plugin-annotations:LATEST"
+                "org.apache.maven.plugin-tools:maven-plugin-annotations:3.3"
         )
     }
 }
